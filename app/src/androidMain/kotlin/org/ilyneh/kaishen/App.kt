09e4e3f7657651b7cbba34add2b7a69e01a7ccba
ilyneh <ilynehan+personal@gmail.com>
2024-11-21 00:30:05 -0500

    Fix: import after module name update

--- app/src/androidMain/kotlin/org/ilyneh/kaishen/App.kt
+++ app/src/androidMain/kotlin/org/ilyneh/kaishen/App.kt
@@ -13,8 +13,8 @@ import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import kaishen.composeapp.generated.resources.Res
-import kaishen.composeapp.generated.resources.compose_multiplatform
+import kaishen.app.generated.resources.Res
+import kaishen.app.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
